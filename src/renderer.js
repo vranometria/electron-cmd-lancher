35983a4722a4a0ac915c7067393dcb5743798b00
vranometria <hizakiri@gmail.com>
2025-04-08 21:53:23 +0900

    ready vue3

--- src/renderer.js
+++ src/renderer.js
@@ -27,5 +27,6 @@
  */
 
 import './index.css';
-
-console.log('👋 This message is being logged by "renderer.js", included via Vite');
+import { createApp } from 'vue';
+import App from './App.vue';
+createApp(App).mount('#app');
