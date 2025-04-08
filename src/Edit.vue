423e2e3062b5823ca6db17f21c07e7e88efb69ae
vranometria <hizakiri@gmail.com>
2025-04-08 22:34:02 +0900

    編集画面のレイアウトを作成

--- src/Edit.vue
+++ src/Edit.vue
@@ -1,3 +1,59 @@
 <template>
-    Edit Screen
+    <div>
+        <input v-model="keyword" placeholder="keyword" />
+    </div>
+    <div>
+        <input v-model="filepath" placeholder="filepath" />
+    </div>
+    <div class="btn-area">
+        <button @click="register">Add</button>
+        <button @click="close">Close</button>
+    </div>
 </template>
+
+<script setup lang="js">
+import { ref } from 'vue';
+
+const keyword = ref('');
+const filepath = ref('');
+const registered = [];
+
+const clear = () => {
+    keyword.value = '';
+    filepath.value = '';
+};
+
+const register = () => {
+    const data = {
+        keyword: keyword.value,
+        filepath: filepath.value,
+    };
+    registered.push(data);
+    clear();
+};
+
+const close = () => {
+
+};
+</script>
+
+<style scoped lang="css">
+.btn-area {
+    width: 100%;
+    display: flex;
+    justify-content: right;
+}
+
+input {
+    width: 100%;
+}
+
+button {
+    margin-top: 3px;
+    width: 45px;
+    height: 40px;
+    border-style: solid 1px black;
+    margin-left: 3px;
+    background-color: white;
+}
+</style>
